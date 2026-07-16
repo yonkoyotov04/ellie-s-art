8a56da21ab877e868f499d8971e9f5a51fe9a28f
yonkoyotov04 <yonkoyotov04@gmail.com>
2026-07-16 10:37:55 +0300

    Added a pool for a PostgreSQL database

---
+++ server/src/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import routes from './routes.js';
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors);
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(routes);
+
+app.listen(2105, '0.0.0.0', () => console.log('Server is listening on port http://localhost:2105.....'));
