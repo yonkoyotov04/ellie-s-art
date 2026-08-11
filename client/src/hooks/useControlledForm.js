717f2357f4998a022f5b1daae937a9bca510b759
yonkoyotov04 <yonkoyotov04@gmail.com>
2026-08-11 10:05:15 +0300

    Created a controlled form custom hook

---
+++ client/src/hooks/useControlledForm.js
@@ -0,0 +1,30 @@
+import { useContext, useEffect, useState } from "react"
+import ErrorContext from "../contexts/ErrorContext.jsx";
+
+export default function useControlledForm(initialValues, onSubmit) {
+    const [values, setValues] = useState(initialValues);
+    const { errorSetter } = useContext(ErrorContext);
+
+    useEffect(() => {
+        setValues(initialValues);
+    }, [initialValues])
+
+    const changeHandler = (e) => {
+        setValues(state => ({...state, [e.target.name]: e.target.value}))
+    }
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+
+        try {
+            await onSubmit(values);
+        } catch (error) {
+            errorSetter(error.message);
+            throw error.message
+        }
+    }
+
+    return {
+        values, changeHandler, submitHandler
+    }
+}
